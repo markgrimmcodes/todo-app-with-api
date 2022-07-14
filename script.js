960eebf973b05405c9e995df3112ab8b8e9018f6
Mark Grimm <mark.grimm.coding@gmail.com>
2022-07-14 14:48:28 +0200

    feat:adds functionality

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const addTodoBtn = document.querySelector("#addButton");
+const removeTodoBtn = document.querySelector("#removeButton");
 const newTodoInput = document.querySelector("#todoText");
 const todoList = document.querySelector("#todoList");
 
@@ -17,13 +18,40 @@ function renderTodos() {
   todoList.innerHTML = "";
   todos.forEach((todo) => {
     const newLi = document.createElement("li");
-    const text = document.createTextNode(todo.description);
-    newLi.appendChild(text);
+    const todoDescription = document.createElement("label");
+    todoDescription.innerText = todo.description;
+
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.id = todo.id;
+    if (todo.done === true) {
+      checkbox.checked = true;
+    }
+
+    checkbox.addEventListener("change", () => {
+      todo.done = !todo.done;
+      const updatedTodo = {
+        id: todo.id,
+        description: todo.description,
+        done: todo.done,
+      };
+      fetch("http://localhost:4730/todos/" + todo.id, {
+        method: "PUT",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify(updatedTodo),
+      }).then((res) => res.json());
+    });
+
+    todoDescription.setAttribute("for", checkbox.id);
+    newLi.append(todoDescription, checkbox);
     todoList.appendChild(newLi);
   });
 }
 
 addTodoBtn.addEventListener("click", () => {
+  if (newTodoInput.value === "") {
+    return;
+  }
   const newTodoText = newTodoInput.value;
   const newTodo = {
     description: newTodoText,
@@ -39,8 +67,23 @@ addTodoBtn.addEventListener("click", () => {
     .then((res) => res.json())
     .then((newTodoFromApi) => {
       todos.push(newTodoFromApi);
+      newTodoInput.value = "";
       renderTodos();
     });
+});
+
+removeTodoBtn.addEventListener("click", () => {
+  for (let todo of todos) {
+    if (todo.done === true) {
+      fetch("http://localhost:4730/todos/" + todo.id, {
+        method: "DELETE",
+      })
+        .then((response) => response.json())
+        .then(() => {
+          loadTodos();
+        });
+    }
+  }
 });
 
 loadTodos();
